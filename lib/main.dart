bddc8dbbff09739f1016efacbf3fa03d72691841
AkritiSubedi <aakusubedi075@gmail.com>
2023-10-13 21:45:23 +0545

    gridview

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,37 @@ class MyApps extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListView(
-          children: [
+        body: GridView.builder(
+          itemCount:64,
+          gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+        itemBuilder: (context, index) => Container(
+          color: Colors.deepPurple,
+          margin: EdgeInsets.all(2),
+          ),
+        ),
+        /* 
+        body: Stack(
+          alignment: Alignment.center,
+          Children:[
             Container(
-              height: 350,
-                color: Colors.deepPurple,
-              ),
-
-              Container(height: 350,
-                color: Colors.deepPurple[400],
-              ),
+              height: 300,
+              width: 300,
+              color: Colors.deepPurple,
+            ),
             Container(
-              height: 350,
-                color: Colors.deepPurple[200],
-              ),
-          ],
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple[400],
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepPurple[200],
+            ),
+          ]
         )
+        */
       ),
     );
   }
