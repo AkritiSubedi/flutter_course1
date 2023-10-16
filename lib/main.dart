03cc5e885514f6ef1951a6189eed2e8a358384ad
AkritiSubedi <aakusubedi075@gmail.com>
2023-10-16 19:06:04 +0545

    gesture detector

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,35 @@
 import 'package:flutter/material.dart';
-// ignore: unused_import
-import 'package:flutter_course1/main.dart';
 
 void main() {
-  runApp(MyApps());
+  runApp(MyApp());
 }
-class MyApps extends StatelessWidget {
-  const MyApps({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // functions and methods
+  void userTapped(){
+    print("User tapped!");
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GridView.builder(
-          itemCount:64,
-          gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-        itemBuilder: (context, index) => Container(
-          color: Colors.deepPurple,
-          margin: EdgeInsets.all(2),
-          ),
-        ),
-        /* 
-        body: Stack(
-          alignment: Alignment.center,
-          Children:[
-            Container(
-              height: 300,
-              width: 300,
-              color: Colors.deepPurple,
-            ),
-            Container(
+        body: Center(
+          child: GestureDetector(
+            onTap: userTapped,
+            child: Container(
               height: 200,
               width: 200,
-              color: Colors.deepPurple[400],
+              color: Colors.deepPurple[300],
+              child: Center(
+                child: Text("Tap me!"),
+              ),
             ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.deepPurple[200],
-            ),
-          ]
-        )
-        */
+          ),
+        ),
       ),
     );
   }
