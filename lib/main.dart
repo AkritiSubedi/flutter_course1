190f084605552e69a81e161b48b48fe20557c6b5
AkritiSubedi <aakusubedi075@gmail.com>
2023-10-28 19:18:50 +0545

    ecommerce_app1

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course1/pages/counter_page.dart';
-import 'package:flutter_course1/pages/first_page.dart';
-import 'package:flutter_course1/pages/home_page.dart';
-import 'package:flutter_course1/pages/settings_page.dart';
-import 'package:flutter_course1/pages/todo_page.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/adapters.dart';
+import 'package:flutter_course1/page/intro_page.dart';
 
-void main() async{
-  //init the hive
-  await Hive.initFlutter();
-
-  //open a box
-  var box = await Hive.openBox('mybox');
+void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: IntroPage(),
     );
   }
 }
