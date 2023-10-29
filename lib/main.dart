4c51250428fa70ded4bc5b4ed59ac74e14e44b67
AkritiSubedi <aakusubedi075@gmail.com>
2023-10-29 19:36:46 +0545

    ecommerce1

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_course1/page/intro_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: IntroPage(),
     );
--- lib/page/intro_page.dart
+++ lib/page/intro_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course1/pages/home_page.dart';
+
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
@@ -8,12 +10,14 @@ class IntroPage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // logo
             Padding(
               padding: const EdgeInsets.all(25.0),
-              child: Image.asset('lib/images/image.png',
-              height: 240,
+              child: Image.asset(
+                'lib/images/image.png',
+                height: 240,
               ),
             ),
             const SizedBox(height: 48),
@@ -23,19 +27,42 @@ class IntroPage extends StatelessWidget {
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 20,
-                ),
+              ),
             ),
-            const SizedBox(height: 24,),
+            const SizedBox(
+              height: 24,
+            ),
             //sub title
-             const Text(
+            const Text(
               'Brand new sneackers and custom kicks made with premium quality',
               style: TextStyle(
                 fontSize: 20,
                 color: Colors.grey,
-                ),
-                textAlign: TextAlign.center,
+              ),
+              textAlign: TextAlign.center,
             ),
+            const SizedBox(height: 8),
             //start new button
+            GestureDetector(
+              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage())),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(25),
+                child: const Center(
+                  child: const Text(
+                    'Shop Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
