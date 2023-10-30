54fe907ee860dcfea314cdc35cda0e2b95471904
AkritiSubedi <aakusubedi075@gmail.com>
2023-10-30 16:14:27 +0545

    ecommerce_aoo_ui2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course1/page/intro_page.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+
+  var myBox = await Hive.openBox('myBoxName');
+
+  myBox.put('key', 'value');
+  var myValue = myBox.get('key');
+
+  myBox.close();
+
   runApp(MyApp());
 }
 
